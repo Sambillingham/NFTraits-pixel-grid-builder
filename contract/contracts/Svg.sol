2d94689da8615e7ca35af154d154fa6dad5c686c
Sambillingham <hello@sambillingham.com>
2022-11-12 14:41:37 +0100

    add multiLayer output

--- contract/contracts/Svg.sol
+++ contract/contracts/Svg.sol
@@ -13,6 +13,12 @@ contract Svg {
     string internal constant WHITE = "FFFFFF";
 	string internal constant BLACK = "000000";
     string internal constant RED = "FF0000";
+    string internal constant GREEN = "00FF00";
+    string internal constant G1 = "111111";
+    string internal constant G2 = "444444";
+    string internal constant G3 = "888888";
+
+    mapping(uint256 => uint256[5]) tokenLayers;
 
     function draw(
 		string memory _svg,
@@ -33,7 +39,12 @@ contract Svg {
 			);
 	}
 
-    function run (uint256 grid) public view returns (string memory){
+
+    function store (uint256 tokenId, uint256[2] calldata layers) public {
+        tokenLayers[tokenId] = layers;
+    }
+    
+    function singleLayer (uint256 grid) public pure returns (string memory){
         bytes1[8] memory bitMask;
         bitMask[0] = (0x7F); // 01111111
         bitMask[1] = (0xBF); // 10111111
@@ -44,16 +55,16 @@ contract Svg {
         bitMask[6] = (0xFD); // 11111101
         bitMask[7] = (0xFE); // 11111110
         
-		uint256 x;
+	    uint256 x;
 		uint256 y;
-        bytes32 _bytes = bytes32(uint256(grid));
+        bytes32 layer = bytes32(uint256(grid));
         
         string memory svg = SVG_HEADER;
         string[256] memory colours;
 
         for (uint256 i; i < 32; i++) {
             for (uint256 c; c < bitMask.length; c++) {
-                colours[(i*8)+c] = (bitMask[c] | bytes1(uint8(_bytes[i])) == bytes1(uint8(0xFF))) ? BLACK : WHITE;
+                colours[(i*8)+c] = (bitMask[c] | bytes1(uint8(layer[i])) == bytes1(uint8(0xFF))) ? BLACK : WHITE;
             }
         }
 
@@ -70,7 +81,7 @@ contract Svg {
         return string.concat(svg, SVG_FOOTER);
     }
 
-    function run2 (uint256 grid, uint256 grid2) public view returns (string memory){
+    function twoLayer (uint256 tokenId) public view returns (string memory){
         bytes1[8] memory bitMask;
         bitMask[0] = (0x7F); // 01111111
         bitMask[1] = (0xBF); // 10111111
@@ -83,18 +94,24 @@ contract Svg {
         
 		uint256 x;
 		uint256 y;
-        bytes32 _bytes = bytes32(uint256(grid));
-        bytes32 _bytes2 = bytes32(uint256(grid2));
+        bytes32 layer1 = bytes32(uint256(tokenLayers[tokenId][0]));
+        bytes32 layer2 = bytes32(uint256(tokenLayers[tokenId][1]));
         
         string memory svg = SVG_HEADER;
         string[256] memory colours;
 
+        uint8 bit1;
+        uint8 bit2;
+
         for (uint256 i; i < 32; i++) {
-            for (uint256 c; c < bitMask.length; c++) {
-                colours[(i*8)+c] = (bitMask[c] | bytes1(uint8(_bytes[i])) == bytes1(uint8(0xFF))) ? BLACK : WHITE;
-                if (bitMask[c] | bytes1(uint8(_bytes2[i])) == bytes1(uint8(0xFF))) {
-                    colours[(i*8)+c] = RED;
-                }
+            for (uint256 b; b < bitMask.length; b++) {
+                bit1 = (bitMask[b] | bytes1(uint8(layer1[i])) == bytes1(uint8(0xFF))) ? 1 : 0;
+                bit2 = (bitMask[b] | bytes1(uint8(layer2[i])) == bytes1(uint8(0xFF))) ? 1 : 0;
+
+                if(bit1 == 0 && bit2 == 0) colours[(i*8)+b] = BLACK;
+                if(bit1 == 1 && bit2 == 1) colours[(i*8)+b] = WHITE;
+                if(bit1 == 0 && bit2 == 1) colours[(i*8)+b] = RED;
+                if(bit1 == 1 && bit2 == 0) colours[(i*8)+b] = GREEN;
             }
         }
 
--- contract/test/test.js
+++ contract/test/test.js
@@ -2,40 +2,60 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const Web3Utils = require('web3-utils');
 
-let grid = [...Array(256)].map( (x,i) => {
+let checkerGrid = [...Array(256)].map( (x,i) => {
   const evenRow = ( Math.floor(i /16)) % 2 == 0;
   if (evenRow) return i % 2 ? 1:0 
   else return i % 2 ? 0:1 ;
 })
 
-console.log(grid)
+const checkerNumber = new Web3Utils.BN(0); 
+checkerGrid.reverse().forEach((n, i) => checkerNumber.setn(i, n));
+
+const grid = [...Array(256)].fill(0);
+const grid2 = [...Array(256)].fill(0);
+grid[0] = 0
+grid2[0] = 0
+
+grid[1] = 1
+grid2[1] = 1
+
+grid[2] = 0
+grid2[2] = 1
+
+grid[3] = 1
+grid2[3] = 0
 
 
 const number = new Web3Utils.BN(0); 
 grid.reverse().forEach((n, i) => number.setn(i, n));
-console.log(number.toString())
 
-const grid2 = [...Array(256)].fill(0);
-grid2[0] = 1
-grid2[15] = 1
-grid2[240] = 1
-grid2[255] = 1
 const number2 = new Web3Utils.BN(0); 
 grid2.reverse().forEach((n, i) => number2.setn(i, n));
 
-describe("svg", function () {
-  it("run svg", async function () {
+describe("svg", async function () {
+
+  it("run single layer", async function () {
     const svgF = await hre.ethers.getContractFactory("Svg");
     const s = await svgF.deploy();
   
     await s.deployed();
   
     console.log("S -> deployed to:", s.address);
-    const svg = await s.run(number.toString());
-    const svg2 = await s.run2(number.toString(), number2.toString());
+    const svg = await s.singleLayer(checkerNumber.toString());
+    console.log(svg)
+  });
 
-    
-    console.log(svg2)
-    expect(s.address).to.equal(s.address)
+  it("run two layer", async function () {
+    const svgF = await hre.ethers.getContractFactory("Svg");
+    const s = await svgF.deploy();
+  
+    await s.deployed();
+  
+    console.log("S -> deployed to:", s.address);
+
+    await s.store( 0,[number.toString(), number2.toString() ]);
+
+    const svg = await s.twoLayer(0);
+    console.log('svg', svg)
   });
 });
